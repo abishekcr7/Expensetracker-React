09848393addcc750ac8c7b698396c10b4e685028
Abishek R <66518962+abishekcr7@users.noreply.github.com>
2023-01-13 12:44:13 +0530

    Add files via upload

---
+++ Expenseitem.js
@@ -0,0 +1,18 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense Item</h2>
+      <div>
+        <p>Food Rs 10</p>
+      </div>
+      <div>
+        <p>Petrol Rs 100</p>
+      </div>
+      <div>
+        <p>Movies Rs 200</p>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
