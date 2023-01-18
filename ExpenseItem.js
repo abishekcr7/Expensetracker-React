296b83868b1b9ada76ae218026e0548f3bd82bef
Abishek R <66518962+abishekcr7@users.noreply.github.com>
2023-01-18 20:21:51 +0530

    added delete button

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,19 +1,22 @@
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
+import "./Expenses.css"
 
 function ExpenseItem(props) {
+  const clickhandler= () =>{
+    console.log("Clicked")
+  }
+  const deleteComp= () =>{
+    console.log("Deleted")
+  }
   return (
     <div className="expense-item">
-      
-      <div>
-        <div>{props.date.toISOString()}</div>
-      </div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <p>{props.location}</p>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <ExpenseDetails amount={props.amount} location={props.location} title={props.title}></ExpenseDetails>
+      <button onClick={clickhandler}>Change Detail</button>
+      <button onClick={deleteComp}>Delete</button>
     </div>
-    
   );
 }
 
