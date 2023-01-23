6c832fcf763e0adc108064e6d70860ea38506751
Abishek R <66518962+abishekcr7@users.noreply.github.com>
2023-01-23 12:48:02 +0530

    Filter feature added

--- Expenses/Expenses.js
+++ Expenses/Expenses.js
@@ -1,31 +1,29 @@
+import { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
 function Expenses(props) {
+  const [filteredYear, setSeletctedYear] = useState("2020");
+  const yearChangeHandler = (selectedyear) => {
+    setSeletctedYear(selectedyear);
+  };
+
+  const filteredExpenses= props.item.filter((expense)=>{
+    return expense.date.getFullYear().toString() === filteredYear
+  })
   return (
     <div className="expenses">
-      <ExpenseItem
-        title={props.item[0].title}
-        amount={props.item[0].amount}
-        location={props.item[0].LocationOfExpenditure}
-        date={props.item[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.item[1].title}
-        amount={props.item[1].amount}
-        location={props.item[1].LocationOfExpenditure}
-        date={props.item[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.item[2].title}
-        amount={props.item[2].amount}
-        location={props.item[2].LocationOfExpenditure}
-        date={props.item[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.item[3].title}
-        amount={props.item[3].amount}
-        location={props.item[3].LocationOfExpenditure}
-        date={props.item[3].date}
-      ></ExpenseItem>
+      <ExpenseFilter
+        selected={filteredYear}
+        onSelectingYear={yearChangeHandler}
+      />
+      {filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.title}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </div>
   );
 }
